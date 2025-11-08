40d89f96e8c5684b524b5da7c9bc7000979c11c3
codedinpeace <vea5a.pranam05@gmail.com>
2025-11-08 12:54:50 +0530

    Connected Database

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const {connectDB} = require('./db/db')
+require('dotenv').config()
 
 const app = express()
+connectDB()
 
 app.get("/", (req,res)=>{
     res.send("Hello World")
