962b56eb4a937765cfa8b1fa12eb15ef01f66aa1
codedinpeace <vea5a.pranam05@gmail.com>
2025-11-23 12:21:13 +0530

    added controllers for deleting and getting resume

--- backend/src/controllers/resume.controllers.js
+++ backend/src/controllers/resume.controllers.js
@@ -24,5 +24,36 @@ const saveResume = async (req,res) =>{
 
 }
 
+const deleteResume = async (req,res)=>{
+    
+    const {id} = req.params 
+
+    try {
+        const resume = await resumeModel.findOne(id)
+        if(!resume) return res.status(400).json({message:"Resume not found"})
+
+            await resume.deleteOne()
+            res.status(200).json({message:"Resume deleted successfully"})
+    } catch (error) {
+        res.status(400).json({message:"Something went wrong"})
+        console.log(error)
+    }
+}
+
+const getResume = async (req,res) =>{
+    
+    const {id} = req.params
+
+    try {
+        const resume = await resumeModel.findOne(id)
+        if(!resume) return res.status(404).json({message:"Resume not found"})
+            res.status(200).json({resume})
+
+    } catch (error) {
+        res.status(400).json({message:"Something went wrong"})
+        console.log(error)
+    }
+}
+
 module.exports = {saveResume}
 
