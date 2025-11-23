fa897333b0f7a5d72a56be3b54f086f627b9a94b
codedinpeace <vea5a.pranam05@gmail.com>
2025-11-23 21:31:25 +0530

    defined routes in frontend

---
+++ frontend/src/LandingPage/LandingPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {HomePage} from '../LandingPage/DirectPage/HomePage'
+import {WhyAptura} from '../LandingPage/DirectPage/WhyAptura'
+import {HowItWorks} from '../LandingPage/DirectPage/HowItWorks'
+import {Testimonials} from '../LandingPage/DirectPage/Testimonials'
+import {CTA} from '../LandingPage/DirectPage/CTA'
+
+import {Navbar} from '../Components/Navbar'
+import {Footer} from '../Components/Footer'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Navbar />
+        <HomePage />
+        <WhyAptura />
+        <HowItWorks />
+        <Testimonials />
+        <CTA />
+        <Footer />
+    </div>
+  )
+}
+
+export default LandingPage
--- frontend/src/Routers/AppRouters.jsx
+++ frontend/src/Routers/AppRouters.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
+import {LandingPage} from '../LandingPage/LandingPage'
+import {CreateResume} from '../LandingPage/RoutingPage/CreateResume'
+import {PrivacyPolicy} from '../LandingPage/RoutingPage/PrivacyPolicy'
+import {Signup} from '../LandingPage/RoutingPage/Signup'
+import {Login} from '../LandingPage/RoutingPage/Login'
+import {OTP} from '../Components/OTP'
+import {Terms} from '../LandingPage/RoutingPage/Terms'
+import {Resume} from '../LandingPage/RoutingPage/Resume'
 
 const AppRouters = () => {
   return (
     <div>
         <Routes>
-            <Route path='/' />
+            <Route path='/' element={<LandingPage />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<Signup />}/>
+            <Route path='/verify' element={<OTP />}/>
+            <Route path='/create-resume' element={<CreateResume />}/>
+            <Route path='/resumes' element={<Resume />}/>
+            <Route path='/terms-of-service' element={<Terms />}/>
+            <Route path='/terms-of-service' element={<PrivacyPolicy />} />
         </Routes>
     </div>
   )
